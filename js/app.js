35fcb8f2bb211829e48b5376cee17e41cff71783
sabbir1020 <gazisabbir924@gamil.com>
2022-10-02 13:52:39 +0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,62 @@
+const selectPlayer = document.getElementById("select-player");
+// console.log(selectPlayer);
+let players = [];
+function selectedPlayer(element) {
+  const playerName = element.parentNode.children[1].children[0].innerText;
+  // const playerName = element.parentNode.children[1].children[0].innerText;
+  console.log(playerName);
+  if (players.length < 5) {
+    players.push(playerName);
+  } else {
+    alert("added 5 players");
+  }
+  selectPlayer.innerHTML = "";
+  //   console.log(selectPlayer);
+  for (const player of players) {
+    const playerName = player;
+    const li = document.createElement("li");
+    li.innerText = playerName;
+    selectPlayer.appendChild(li);
+  }
+}
+
+// function enentvalue ()
+document
+  .getElementById("calculator-btn")
+  .addEventListener("click", function () {
+    const perAmountElement = document.getElementById("per-amount").value;
+    perAmountElement.value = "";
+    const perAmount = parseInt(perAmountElement);
+    if (isNaN(perAmount)) {
+      alert("Please enter your number");
+      return;
+    }
+    const playerTotal = players.length;
+    // const playerExpenses = perAmount * playerTotal;
+    // multValue(perAmount, playerTotal);
+    // setValue("total-cost", playerExpenses);
+    const multi = multValue(perAmount, playerTotal);
+    setValue("total-cost", multi);
+  });
+document
+  .getElementById("total-calculate-btn")
+  .addEventListener("click", function () {
+    const perAmountElement = document.getElementById("per-amount").value;
+    perAmountElement.value = "";
+    const perAmount = parseInt(perAmountElement);
+    const playerTotal = players.length;
+    const perAmountAndPlayer = perAmount * playerTotal;
+
+    const managerInputElement = document.getElementById("Manager-input").value;
+    const managerInput = parseInt(managerInputElement);
+    const coachInputElement = document.getElementById("coach-input").value;
+    const coachInput = parseInt(coachInputElement);
+
+    const total = managerInput + coachInput + perAmountAndPlayer;
+    if (isNaN(total)) {
+      alert("Please valid Number");
+      return;
+    }
+    setValue("total", total);
+    // console.log(total);
+  });
---
+++ js/function.js
@@ -0,0 +1,8 @@
+function setValue(elementId, value) {
+  const amount = document.getElementById(elementId, value);
+  amount.innerText = value;
+}
+function multValue(perAmount, playerTotal) {
+  const totalValue = perAmount * playerTotal;
+  return totalValue;
+}
